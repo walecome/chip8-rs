25fdf6f52f9cd94140bcabc43c8e7214dca16e9d
Alexander Wilkens <alexander.wilkens96@gmail.com>
2023-09-09 13:46:51 +0200

    Display things on screen

--- src/chip8/display.rs
+++ src/chip8/display.rs
@@ -1,17 +1,25 @@
 pub struct Display {
-    width: usize,
-    height: usize,
+    pub width: u32,
+    pub height: u32,
     data: Vec<bool>,
 }
 
 impl Display {
     pub fn new() -> Display {
-        let width: usize = 64;
-        let height: usize = 32;
+        let width: u32 = 64;
+        let height: u32 = 32;
         Display {
             width,
             height,
-            data: vec![false; width * height],
+            data: vec![false; (width * height) as usize],
         }
+    }
+
+    pub fn get(&self, x: u32, y: u32) -> bool {
+        return self.data[(y * self.width + x) as usize];
+    }
+
+    pub fn set(&mut self, x: u32, y: u32, value: bool) {
+        self.data[(y * self.width + x) as usize] = value;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,110 @@
-mod chip8;
-use chip8::cpu::Cpu;
+pub mod chip8;
 
+use chip8::display::Display;
+
+extern crate sdl2;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::Point;
+use std::time::Duration;
+
+struct GridMapper {
+    cell_size: u32,
+    width: u32,
+    height: u32,
+    divider_size: u32,
+}
+
+enum GridPixelType {
+    Divider,
+    Cell { x: u32, y: u32 },
+}
+
+impl GridMapper {
+    fn new(grid_width: u32, grid_height: u32, cell_size: u32) -> GridMapper {
+        let divider_size: u32 = 1;
+        let width = (grid_width * cell_size) + (divider_size * (grid_width - 1));
+        let height = (grid_height * cell_size) + (divider_size * (grid_height - 1));
+
+        return GridMapper {
+            cell_size,
+            width,
+            height,
+            divider_size,
+        };
+    }
+
+    fn get_type(&self, x: u32, y: u32) -> GridPixelType {
+        let chunk_size = self.cell_size + self.divider_size;
+        let x_hits_divider = x % chunk_size == 0;
+        let y_hits_divider = y % chunk_size == 0;
+
+        return if x_hits_divider || y_hits_divider {
+            GridPixelType::Divider
+        } else {
+            GridPixelType::Cell {
+                x: x / chunk_size,
+                y: y / chunk_size,
+            }
+        };
+    }
+}
 
 fn main() {
-    let mut cpu = Cpu::new();
-    loop {
-        let raw_instruction = cpu.fetch();
-        let instruction = cpu.decode(raw_instruction);
-        cpu.execute(instruction);
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let mut display = Display::new();
+    let grid_mapper = GridMapper::new(display.width, display.height, 15);
+    let window = video_subsystem
+        .window("rust-sdl2 demo", grid_mapper.width, grid_mapper.height)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+        for x in 0..grid_mapper.width {
+            for y in 0..grid_mapper.height {
+                let color = match grid_mapper.get_type(x, y) {
+                    GridPixelType::Cell {x, y} => {
+                        if display.get(x, y) {
+                            Color::RGB(255, 255, 255)
+                        } else {
+                            Color::RGB(0, 0, 0)
+                        }
+                    },
+                    GridPixelType::Divider => Color::RGB(50, 50, 50),
+                };
+                canvas.set_draw_color(color);
+                canvas.draw_point(Point::new(x as i32, y as i32)).unwrap();
+            }
+        }
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
