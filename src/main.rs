75b03d0e5a6bbbbe3f83fbe17e3bc15037fb694b
Alexander Wilkens <alexander.wilkens96@gmail.com>
2023-09-10 18:22:49 +0200

    Draw to texture instead of points

--- src/main.rs
+++ src/main.rs
@@ -8,55 +8,13 @@ extern crate sdl2;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use sdl2::rect::Point;
+use sdl2::pixels::PixelFormatEnum;
 
 use clap::Parser;
 
 use std::time::Instant;
 
 use std::fs;
-
-struct GridMapper {
-    cell_size: u32,
-    width: u32,
-    height: u32,
-    divider_size: u32,
-}
-
-enum GridPixelType {
-    Divider,
-    Cell { vram_x: u8, vram_y: u8 },
-}
-
-impl GridMapper {
-    fn new(grid_width: u32, grid_height: u32, cell_size: u32) -> GridMapper {
-        let divider_size: u32 = 1;
-        let width = (grid_width * cell_size) + (divider_size * (grid_width - 1));
-        let height = (grid_height * cell_size) + (divider_size * (grid_height - 1));
-
-        return GridMapper {
-            cell_size,
-            width,
-            height,
-            divider_size,
-        };
-    }
-
-    fn get_type(&self, x: u32, y: u32) -> GridPixelType {
-        let chunk_size = self.cell_size + self.divider_size;
-        let x_hits_divider = x % chunk_size == 0;
-        let y_hits_divider = y % chunk_size == 0;
-
-        return if x_hits_divider || y_hits_divider {
-            GridPixelType::Divider
-        } else {
-            GridPixelType::Cell {
-                vram_x: (x / chunk_size) as u8,
-                vram_y: (y / chunk_size) as u8,
-            }
-        };
-    }
-}
 
 #[derive(Parser)]
 struct Args {
@@ -66,37 +24,56 @@ struct Args {
     use_copy_shift: bool,
 }
 
-fn main() {
+pub fn main() -> Result<(), String> {
     let args = Args::parse();
     let rom_data = fs::read(args.rom).unwrap();
-
     let mut cpu = Cpu::new(Memory::new(rom_data), args.use_copy_shift);
 
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
 
-    let grid_mapper = GridMapper::new(cpu.vram().width as u32, cpu.vram().height as u32, 15);
     let window = video_subsystem
-        .window("rust-sdl2 demo", grid_mapper.width, grid_mapper.height)
+        .window("Chip-8 emulator", 1600, 800)
         .position_centered()
+        .opengl()
         .build()
-        .unwrap();
+        .map_err(|e| e.to_string())?;
 
-    let mut canvas = window.into_canvas().build().unwrap();
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+    let texture_creator = canvas.texture_creator();
 
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    fn write_pixel(buffer: &mut [u8], pitch: usize, x: usize, y: usize, color: Color) {
+        let offset = y * pitch + x * 3;
+        buffer[offset] = color.r;
+        buffer[offset + 1] = color.g;
+        buffer[offset + 2] = color.b
+    }
+
+    let mut texture = texture_creator
+        .create_texture_streaming(
+            PixelFormatEnum::RGB24,
+            cpu.vram().width as u32,
+            cpu.vram().height as u32,
+        )
+        .map_err(|e| e.to_string())?;
+    // Create a red-green gradient
+    texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
+        for y in 0..cpu.vram().height {
+            for x in 0..cpu.vram().width {
+                write_pixel(buffer, pitch, x as usize, y as usize, Color::RGB(0, 0, 0));
+            }
+        }
+    })?;
+
     canvas.clear();
+    canvas.copy(&texture, None, None)?;
     canvas.present();
 
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
-    'running: loop {
+    let mut event_pump = sdl_context.event_pump()?;
 
+    'running: loop {
         let main_loop_start = Instant::now();
 
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(0, 0, 0));
-        canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
@@ -107,10 +84,9 @@ fn main() {
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
 
-        let cpu_start = Instant::now();
         // Tick CPU
+        let cpu_start = Instant::now();
         let raw_instruction = cpu.fetch();
         let instruction = cpu.decode(raw_instruction);
         println!("{:#06X} -> {:?}", raw_instruction, instruction);
@@ -118,32 +94,33 @@ fn main() {
         let cpu_duration = cpu_start.elapsed();
         println!("CPU time: {:?}", cpu_duration);
 
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
 
-        let draw_start = Instant::now();
         // Draw VRAM
-        for x in 0..grid_mapper.width {
-            for y in 0..grid_mapper.height {
-                let color = match grid_mapper.get_type(x, y) {
-                    GridPixelType::Cell { vram_x, vram_y } => {
-                        if cpu.vram().get_cell(vram_x, vram_y) {
-                            Color::RGB(255, 255, 255)
-                        } else {
-                            Color::RGB(0, 0, 0)
-                        }
-                    }
-                    GridPixelType::Divider => Color::RGB(50, 50, 50),
-                };
-                canvas.set_draw_color(color);
-                canvas.draw_point(Point::new(x as i32, y as i32)).unwrap();
+        let draw_start = Instant::now();
+        texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
+            for y in 0..cpu.vram().height {
+                for x in 0..cpu.vram().width {
+                    let color = if cpu.vram().get_cell(x, y) {
+                        Color::RGB(255, 255, 255)
+                    } else {
+                        Color::RGB(0, 0, 0)
+                    };
+                    write_pixel(buffer, pitch, x as usize, y as usize, color);
+                }
             }
-        }
+        })?;
+
+        canvas.copy(&texture, None, None)?;
         canvas.present();
 
         let draw_duration = draw_start.elapsed();
         println!("Draw time: {:?}", draw_duration);
 
-
         let main_loop_duration = main_loop_start.elapsed();
         println!("Main loop time: {:?}", main_loop_duration);
     }
+
+    Ok(())
 }
